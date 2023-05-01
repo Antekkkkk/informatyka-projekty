41ef7dc359a52275d58c5c1ddd569241fff78538
Antekkkkk <antek.kukowski1@gmail.com>
2023-05-01 20:10:04 +0200

    dodanie klasy XYZ

--- projekt1.py
+++ projekt1.py
@@ -1,29 +1,56 @@
-with open([(plik)], 'r') as plik:
-    linie = plik.readlines()
-    Xd = []
-    Yd = []
-    Zd = []
-    fd = []
-    ld = []
-    hd = []
-    typ = str(linie[0].split())
-    # print(type(typ), 'typ typu')
-    elip = list(str(linie[1]))
-    print(elip, 'elip')
-    dane = linie[2:]
-    # print(typ, 'typ')
-    typ = list(typ)
-    if 'X' in typ:
-        print('działa')
-    if 'X' in typ:
-        for linia in dane:
-            linia = linia.split()
-            Xd.append(float(linia[0]))
-            Yd.append(float(linia[1]))
-            Zd.append(float(linia[2]))
-    elif 'f' in typ:
-        for linia in dane:
-            linia = linia.split()
-            fd.append(float(linia[0]) * np.pi / 180)
-            ld.append(float(linia[1]) * np.pi / 180)
-            hd.append(float(linia[2]) * np.pi / 180)
+if True:
+    with open([(plik)], 'r') as plik:
+        linie = plik.readlines()
+        Xd = []
+        Yd = []
+        Zd = []
+        fd = []
+        ld = []
+        hd = []
+        typ = str(linie[0].split())
+        # print(type(typ), 'typ typu')
+        elip = list(str(linie[1]))
+        print(elip, 'elip')
+        dane = linie[2:]
+        # print(typ, 'typ')
+        typ = list(typ)
+        if 'X' in typ:
+            print('działa')
+        if 'X' in typ:
+            for linia in dane:
+                linia = linia.split()
+                Xd.append(float(linia[0]))
+                Yd.append(float(linia[1]))
+                Zd.append(float(linia[2]))
+        elif 'f' in typ:
+            for linia in dane:
+                linia = linia.split()
+                fd.append(float(linia[0]) * np.pi / 180)
+                ld.append(float(linia[1]) * np.pi / 180)
+                hd.append(float(linia[2]) * np.pi / 180)
+
+
+    class XYZ:
+        a = 6378137
+        e2 = 0.00669438002290
+
+        def __init__(self, X, Y, Z):
+            self.X = X
+            self.Y = Y
+            self.Z = Z
+
+        def XYZ2flh(self):
+            X = self.X
+            Y = self.Y
+            Z = self.Z
+            f, l, h = gw.hirvonen(X, Y, Z, a, e2)
+            self.fi = f
+            self.la = l
+            self.h = h
+            return (self.fi, self.la, self.h)
+
+        def XYZ2neu(self):
+            dXYZ = [self.X, self.Y, self.Z]
+            f, l, h = gw.hirvonen(self.X, self.Y, self.Z, a, e2)
+            self.neu = gw.XYZ2neu(dXYZ, f, l)
+            return (self.neu)
